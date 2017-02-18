3bedb7733dcbd219c103b3b7752127cdd07a27fe
George Kaimakis <geomatus@mac.com>
2017-02-18 21:35:16 +0000

    Adds parent class - robot:

---
+++ Ch.01/robot.py
@@ -0,0 +1,24 @@
+# parent class:
+
+class Robot(object):
+
+    def __init__(self):
+        pass
+
+    def set_description(self, desc):
+        self.description = desc
+
+    def get_description(self):
+        pass
+
+    def set_move_behavior(self, mb):
+        self.move_behavior = mb
+
+    def set_sense_behavior(self, sb):
+        self.sense_behavior = sb
+
+    def perform_move(self):
+        self.move_behavior.move()
+
+    def perform_sense(self):
+        self.sense_behavior.sense()
